47d85d8509ddf2b2589e416d9c03ad6f1c35c7f3
quatroimetmoi <luutranvanan@gmail.com>
2026-02-26 18:50:07 +0700

    Update and rename JsonFactory.py to Dataset/JsonFactory.py

--- Dataset/JsonFactory.py
+++ Dataset/JsonFactory.py
@@ -8,4 +8,5 @@ class JsonFileFactory:
     @staticmethod
     def write_data(path, data):
         with open(path, "w", encoding="utf-8") as f:
+
             json.dump(data, f, ensure_ascii=False, indent=4)
